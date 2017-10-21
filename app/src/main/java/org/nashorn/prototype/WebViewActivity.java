1a84ca2ffea32f7638e5c80545265458e4058508
Jeong kum ho <nashorn74@gmail.com>
2017-10-21 14:00:56 +0900

    웹 페이지 스키마 호출 처리 추가

--- app/src/main/java/org/nashorn/prototype/WebViewActivity.java
+++ app/src/main/java/org/nashorn/prototype/WebViewActivity.java
@@ -4,11 +4,16 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.webkit.JsResult;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 
 public class WebViewActivity extends AppCompatActivity {
     private static final String HOME_URL = "http://10.0.2.2:9000/#!/";
@@ -34,6 +39,47 @@ public class WebViewActivity extends AppCompatActivity {
         }
     }
 
+    class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            Log.i("URL=", url);
+            String urls[] = url.split(":");
+            if (urls[1].equals("detail")) {
+                String params[] = urls[2].split("&");
+                Log.i("urls[2]",urls[2]);
+                try {
+                    params[1] = URLDecoder.decode(params[1], "utf-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                Log.i("params[0]",params[0]);
+                Log.i("params[1]",params[1]);
+                Log.i("params[2]",params[2]);
+                return true;
+            } else if (url.equals("login:")) {
+                AlertDialog.Builder loginDialog =
+                        new AlertDialog.Builder(WebViewActivity.this);
+                loginDialog.setTitle("Login");
+                loginDialog.setMessage("아이디와 비밀번호를 입력하세요.");
+                loginDialog.setPositiveButton("로그인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        webView.loadUrl(USERLIST_URL);
+                    }
+                });
+                loginDialog.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        webView.loadUrl(HOME_URL);
+                    }
+                });
+                loginDialog.show();
+                return true;
+            }
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +87,9 @@ public class WebViewActivity extends AppCompatActivity {
         webView = (WebView)findViewById(R.id.webview);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
         webView.setWebChromeClient(new WebBrowserClient());
+        webView.setWebViewClient(new MyWebViewClient());
         webView.loadUrl(HOME_URL);
     }
 
