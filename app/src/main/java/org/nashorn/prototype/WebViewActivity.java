1424f1ae38722af453e6ec4173c7dc6acf98065c
Jeong kum ho <nashorn74@gmail.com>
2017-10-28 11:45:52 +0900

    비로그인 처리 추가

--- app/src/main/java/org/nashorn/prototype/WebViewActivity.java
+++ app/src/main/java/org/nashorn/prototype/WebViewActivity.java
@@ -21,14 +21,21 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Iterator;
 
 public class WebViewActivity extends AppCompatActivity {
     private static final String HOME_URL = "http://10.0.2.2:9000/#!/";
@@ -132,6 +139,13 @@ public class WebViewActivity extends AppCompatActivity {
             startActivity(intent);
             finish();
         }
+
+        //교재 p707~p713 Firebase 설정 적용
+        //Registration ID
+        String regId = FirebaseInstanceId.getInstance().getToken();
+        Log.i("regId", regId);
+        new Nologin().execute(
+                "http://172.16.1.248:52273/user/nologin", regId);
     }
     //로그아웃
     public void logout(View view) {
@@ -214,5 +228,88 @@ public class WebViewActivity extends AppCompatActivity {
             } catch (Exception e) { e.printStackTrace(); }
             return output.toString();
         }
+    }
+
+    class Nologin extends AsyncTask<String,String,String> {
+        ProgressDialog dialog = new ProgressDialog(WebViewActivity.this);
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder output = new StringBuilder();
+            try {
+                URL url = new URL(params[0]);
+                JSONObject postDataParams = new JSONObject();
+                postDataParams.put("device_token", params[1]);
+
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                if (conn != null) {
+                    conn.setConnectTimeout(10000);
+                    conn.setRequestMethod("POST");
+                    conn.setDoInput(true); conn.setDoOutput(true);
+                    OutputStream os = conn.getOutputStream();
+                    BufferedWriter writer = new BufferedWriter(
+                            new OutputStreamWriter(os, "UTF-8"));
+                    writer.write(getPostDataString(postDataParams));
+                    writer.flush();
+                    writer.close();
+                    os.close();
+                    BufferedReader reader = new BufferedReader(
+                            new InputStreamReader(conn.getInputStream()));
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) break;
+                        output.append(line);
+                    }
+                    reader.close();
+                    conn.disconnect();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+            return output.toString();
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("로그인 중...");
+            dialog.show();
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            dialog.dismiss();
+            try {
+                JSONObject json = new JSONObject(s);
+                if (json.getBoolean("result") == true) {//로그인 성공
+                } else {//로그인 실패
+                    Toast.makeText(WebViewActivity.this,
+                            json.getString("err"),
+                            Toast.LENGTH_SHORT).show();
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
+    public String getPostDataString(JSONObject params) throws Exception {
+
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+
+        Iterator<String> itr = params.keys();
+
+        while(itr.hasNext()){
+
+            String key= itr.next();
+            Object value = params.get(key);
+
+            if (first)
+                first = false;
+            else
+                result.append("&");
+
+            result.append(URLEncoder.encode(key, "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(value.toString(), "UTF-8"));
+
+        }
+        return result.toString();
     }
 }
